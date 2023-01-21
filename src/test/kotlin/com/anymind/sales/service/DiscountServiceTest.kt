28a0f63d7626e757605278cd19a5bd6fcc81ae39
ali.al-iusefi <alighanem99@live.com>
2023-01-22 00:12:56 +0100

    discount and point service test

--- src/test/kotlin/com/anymind/sales/service/DiscountServiceTest.kt
+++ src/test/kotlin/com/anymind/sales/service/DiscountServiceTest.kt
@@ -1,21 +1,53 @@
 package com.anymind.sales.service
 
+import com.anymind.sales.config.ValidDiscountIntervalConfiguration
+import com.anymind.sales.entity.PaymentMethod
+import com.anymind.sales.exception.BadRequestException
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class DiscountServiceTest {
-
     @Test
-    fun shouldReturnDiscountedPriceWhenPassingValidPriceAndPaymentMethod() {
+    fun shouldNotThrowExceptionWhenPassingValidArguments() {
+        val config = ValidDiscountIntervalConfiguration(
+            modifier = buildMap {
+                put(
+                    PaymentMethod.CASH,
+                    DiscountService.ValidDiscountIntervalConfig(0.5, 0.9)
+                )
+            }
+        )
+        val service = DiscountService(config)
 
+        service.validatePriceModifierForPaymentMethod(0.6, PaymentMethod.CASH)
     }
 
     @Test
-    fun shouldThrowIllegalArgumentExceptionWhenConfiguredIncorrectly() {
+    fun shouldThrowIllegalArgumentExceptionWhenMappedIncorrectly() {
+        val config = ValidDiscountIntervalConfiguration(
+            modifier = buildMap {}
+        )
+        val service = DiscountService(config)
 
+        assertThrows<IllegalArgumentException> {
+            service.validatePriceModifierForPaymentMethod(0.6, PaymentMethod.CASH)
+        }
     }
 
     @Test
     fun shouldThrowBadRequestExceptionWhenPriceModifierDoesntFitInterval() {
+        val config = ValidDiscountIntervalConfiguration(
+            modifier = buildMap {
+                put(
+                    PaymentMethod.CASH,
+                    DiscountService.ValidDiscountIntervalConfig(0.5, 0.9)
+                )
+            }
+        )
+        val service = DiscountService(config)
 
+        assertThrows<BadRequestException> {
+            service.validatePriceModifierForPaymentMethod(7.0, PaymentMethod.CASH)
+        }
     }
 }
--- src/test/kotlin/com/anymind/sales/service/PointServiceTest.kt
+++ src/test/kotlin/com/anymind/sales/service/PointServiceTest.kt
@@ -1,16 +1,60 @@
 package com.anymind.sales.service
 
+import com.anymind.sales.config.PointMultiplierConfiguration
+import com.anymind.sales.entity.PaymentMethod
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import java.math.BigDecimal
 
 class PointServiceTest {
 
     @Test
-    fun shouldReturnPointsWhenPassingValidArguments() {
+    fun shouldReturnPointsMultiplierWhenPassingValidArguments() {
+        val multiplier = 0.05
+        val config = PointMultiplierConfiguration(
+            multiplier = buildMap {
+                put(
+                    PaymentMethod.CASH,
+                    multiplier
+                )
+            }
+        )
+        val service = PointService(config)
 
+        val actualMultiplier = service.getPointMultiplier(PaymentMethod.CASH)
+
+        assertThat(multiplier).isEqualTo(actualMultiplier)
     }
 
     @Test
-    fun shouldReturnAggregatedHourlySales() {
+    fun shouldCalculatePointsWhenPassingValidArguments() {
+        val multiplier = 0.50
+        val config = PointMultiplierConfiguration(
+            multiplier = buildMap {
+                put(
+                    PaymentMethod.CASH,
+                    multiplier
+                )
+            }
+        )
+        val service = PointService(config)
 
+        val points = service.calculatePoints(BigDecimal(2.0), PaymentMethod.CASH)
+
+        val expectedPoints = 1
+        assertThat(expectedPoints).isEqualTo(points)
+    }
+
+    @Test
+    fun shouldThrowIllegalArgumentExceptionWhenMappedIncorrectly() {
+        val config = PointMultiplierConfiguration(
+            multiplier = buildMap {}
+        )
+        val service = PointService(config)
+
+        assertThrows<IllegalArgumentException> {
+            service.getPointMultiplier(PaymentMethod.CASH)
+        }
     }
 }
